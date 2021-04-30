97d0f5e309cc258a0d035f84c2153f5273a73c27
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-30 16:08:11 -0700

    DEFAULT_INSTALLS_FULL_V1.sh
    An archive of the first version of the default program install script for Ubuntu 20.04.

---
+++ Install/DefaultPrograms/Linux/Ubuntu/20.04/OldVersions/DEFAULT_INSTALLS_FULL_V1.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Full install of all default programs available in Skeuwie. Other scripts will let you choose what you want to install. This script is for people who select "select all"
+# Start of script
+sudo su;
+sudo apt-get vlc-media-player;
+sudo apt-get libreoffice;
+sudo apt-get okular;
+sudo apt-get kclock;
+sudo apt-get virtualbox;
+sudo apt-get spm-camcamplus; # The only included software developed by me
+sudo apt-get konsole;
+sudo apt-get konquerer;
+sudo apt-get skeuwie-skin-3; # The package that reskins default programs
+break;
+exit;
+# File info
+# File type: Bourne Again SHell script file (*.sh)
+# File version: 1 (Friday, April 30th 2021 at 4:07 pm
+# Line count (including blank lines and compiler line): 21
+# End of script
