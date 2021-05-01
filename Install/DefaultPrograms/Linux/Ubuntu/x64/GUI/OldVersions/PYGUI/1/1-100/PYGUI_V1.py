156228c40aea1e95824957509fc579d651424dc3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-30 18:03:46 -0700

    PYGUI_V1.py
    An archive of the first version of the Ubuntu user interface for Skeuowie.

---
+++ Install/DefaultPrograms/Linux/Ubuntu/x64/GUI/OldVersions/PYGUI/1/1-100/PYGUI_V1.py
@@ -0,0 +1,57 @@
+# Start of script
+# //Skeuwie/Install/DefaultPrograms/Linux/Ubuntu/x64/GUI/PYGUI.md
+# The all-in-one GUI tool for 64 bit Ubuntu (From Ubuntu 4.10 and above, given you select the 64 bit variant)
+""" Divider """"
+# Please note that I am good at writing command line python scripts, but not too great at creating a GUI in python, this is something I am still trying to learn, and currently is a challenge to me due to a lack of time and resources
+''' Divider '''
+# Import section
+@import_section # Experimental
+import webgl()
+import canvas()
+import random()
+import threeD()
+@class_section # Experimental
+class skeuowiePythonGUI_ForUbuntu2004
+maxAttempts = int(10)
+minAttempts = int(1)
+@uname # Experimental
+uname_OS = str("Ubuntu 20.04")
+uname_KERNEL = str("Linux Kernel v5.4")
+# Definition section
+@definitions_section # Experimental
+def guiBody():
+
+def webGL():
+  print ("WebGL suppot
+  # return break;
+def ender(): # Switches from GUI to CLI 
+# Drawing the GUI
+def log():
+	# Logs input for future reference
+	input1 = str(startHere)
+	input2 = str(noMore)
+@start # Experimental
+return webGL()
+return guiBody()
+@header # Experimental
+print ("Skeuowie GUI for 64 bit Ubuntu and Debian")
+startHere = str(input("Would you like to launch the program? (Y/N)"))
+@log startHere()
+startHere == startHere.upper() # Makes input all uppercase, to handle queries better
+if (startHere == "Y" or "YES"):
+	return guiBodyMain()
+	print ("The GUI is currently unavailable")
+else:
+	print ("You have chosen not to display the GUI. This program will now quit")
+	return ender()
+print ("GUI is incomplete")
+noMore = input("Press [ENTER] key to quit")
+@log noMore()
+print ("The program should no longer be running. If it still is, press the close button. If this doesn't work, try ending the task/process with a task/process manager")
+break
+""" File info
+File type: Python 3 source file (*.py)
+File version: 1 (Friday, April 30th 2021 at 6:03 pm)
+Line count (including blank lines and compiler line): 58
+"""
+# End of script
